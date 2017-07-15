cc50276ea168c4c44126a507a16463cc1b3bd59f
Julie Anderson <juliemanderson@gmail.com>
2017-07-15 10:10:51 -0500

    Add scale

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,46 +1,58 @@
-/**
- * Created by julieanderson on 7/15/17.
- */
-var dataset = [9,2,3,4,2,6,4,8,9];
-var w = 200;
-var h = 100;
-var barPadding = 4;
+//Width and height
+var w = 500;
+var h = 300;
+var padding = 20;
 
+var dataset = [
+    [5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
+    [410, 12], [475, 44], [25, 67], [85, 21], [220, 88], [500, 200]
+];
+
+var xScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset, function(d) { return d[0]; })])
+    .range([padding, w-padding*2]);
+
+var yScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset, function(d) { return d[1]; })])
+    .range([h-padding, padding]);
+
+var rScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset, function(d) { return d[1]; })])
+    .range([2,5]);
+
+//Create SVG element
 var svg = d3.select("body")
     .append("svg")
     .attr("width", w)
     .attr("height", h);
 
-svg.selectAll("rect")
+svg.selectAll("circle")
     .data(dataset)
     .enter()
-    .append("rect")
-    .attr("x", function(d,i){
-        return i * (w / dataset.length);
+    .append("circle")
+    .attr("cx", function(d) {
+        return xScale(d[0]);
     })
-    .attr("y", function(d){
-        return h-(d* 10);
+    .attr("cy", function(d) {
+        return yScale(d[1]);
     })
-    .attr("width", w / dataset.length - barPadding)
-    .attr("height", function(d){
-        return d * 10;
-    })
-    .attr("fill", "#e51b5e");
+    .attr("r", function(d) {
+        return rScale(d[1]);
+    });
 
 svg.selectAll("text")
     .data(dataset)
     .enter()
     .append("text")
-    .text(function(d){
-        return d;
+    .text(function(d) {
+        return d[0] + "," + d[1];
     })
-    .attr("x", function(d, i) {
-        return i * (w / dataset.length)+ (w/ dataset.length - barPadding) /2;
+    .attr("x", function(d) {
+        return xScale(d[0]);
     })
     .attr("y", function(d) {
-        return h - (d * 10)+14;
+        return yScale(d[1]);
     })
     .attr("font-family", "sans-serif")
     .attr("font-size", "11px")
-    .attr("fill", "white")
-    .attr("text-anchor", "middle");
+    .attr("fill", "red");
